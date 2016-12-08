bc4eb5a3172c3686c7585207a672b6cd79d7c9f9
Jett Farmer <jett@app-order.com>
2016-12-08 10:56:53 -0800

    When the first letter of a contacts name is an emoji the application crashes because the `UnicodeScalar` returns nil. This is now fixed.

--- Pods/EPExtensions.swift
+++ Pods/EPExtensions.swift
@@ -25,6 +25,9 @@ extension String {
     func containsAlphabets() -> Bool {
         //Checks if all the characters inside the string are alphabets
         let set = CharacterSet.letters
-        return self.utf16.contains( where: { return set.contains(UnicodeScalar($0)!)  } )
+        return self.utf16.contains( where: {
+            guard let unicode = UnicodeScalar($0) else { return false }
+            return set.contains(unicode)
+        })
     }
 }
