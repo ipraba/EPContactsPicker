aae9af70192f899f034cfb040a09705ddbd8aa6b
Sami Samhuri <sami@samhuri.net>
2017-05-14 00:18:00 -0700

    add a shared framework target for Carthage

---
+++ EPContactsPicker/EPContactsPicker.h
@@ -0,0 +1,19 @@
+//
+//  EPContactsPicker.h
+//  EPContactsPicker
+//
+//  Created by Sami Samhuri on 2017-05-14.
+//  Copyright © 2017 Prabaharan Elangovan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EPContactsPicker.
+FOUNDATION_EXPORT double EPContactsPickerVersionNumber;
+
+//! Project version string for EPContactsPicker.
+FOUNDATION_EXPORT const unsigned char EPContactsPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EPContactsPicker/PublicHeader.h>
+
+
