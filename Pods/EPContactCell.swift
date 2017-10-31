b32ceed23c7d2d72ad525c6c76d8e2dbd6cf7150
hilla <hilla@bens-MacBook-Pro.local>
2017-10-31 18:32:38 +0200

    cell design

--- Pods/EPContactCell.swift
+++ Pods/EPContactCell.swift
@@ -31,17 +31,19 @@ class EPContactCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
     }
     
-    func updateInitialsColorForIndexPath(_ indexpath: IndexPath) {
-        //Applies color to Initial Label
-        let colorArray = [EPGlobalConstants.Colors.amethystColor,EPGlobalConstants.Colors.asbestosColor,EPGlobalConstants.Colors.emeraldColor,EPGlobalConstants.Colors.peterRiverColor,EPGlobalConstants.Colors.pomegranateColor,EPGlobalConstants.Colors.pumpkinColor,EPGlobalConstants.Colors.sunflowerColor]
-        let randomValue = (indexpath.row + indexpath.section) % colorArray.count
-        contactInitialLabel.backgroundColor = colorArray[randomValue]
-    }
+//    func updateInitialsColorForIndexPath(_ indexpath: IndexPath) {
+//        //Applies color to Initial Label
+//        let colorArray = [EPGlobalConstants.Colors.amethystColor,EPGlobalConstants.Colors.asbestosColor,EPGlobalConstants.Colors.emeraldColor,EPGlobalConstants.Colors.peterRiverColor,EPGlobalConstants.Colors.pomegranateColor,EPGlobalConstants.Colors.pumpkinColor,EPGlobalConstants.Colors.sunflowerColor]
+//        let randomValue = (indexpath.row + indexpath.section) % colorArray.count
+//        contactInitialLabel.backgroundColor = colorArray[randomValue]
+//    }
  
     func updateContactsinUI(_ contact: EPContact, indexPath: IndexPath, subtitleType: SubtitleCellValue) {
         self.contact = contact
         //Update all UI in the cell here
+        self.backgroundColor = UIColor.clear
         self.contactTextLabel?.text = contact.displayName()
+        self.contactTextLabel.textColor = UIColor.white
         updateSubtitleBasedonType(subtitleType, contact: contact)
         if contact.thumbnailProfileImage != nil {
             self.contactImageView?.image = contact.thumbnailProfileImage
@@ -49,7 +51,8 @@ class EPContactCell: UITableViewCell {
             self.contactInitialLabel.isHidden = true
         } else {
             self.contactInitialLabel.text = contact.contactInitials()
-            updateInitialsColorForIndexPath(indexPath)
+            self.contactInitialLabel.textColor = UIColor.white
+            self.contactInitialLabel.backgroundColor = EPGlobalConstants.Colors.lightGrey
             self.contactImageView.isHidden = true
             self.contactInitialLabel.isHidden = false
         }
@@ -70,6 +73,7 @@ class EPContactCell: UITableViewCell {
             }
             else {
                 self.contactDetailTextLabel.text = EPGlobalConstants.Strings.phoneNumberNotAvaialable
+                self.contactDetailTextLabel.textColor = EPGlobalConstants.Colors.lightGrey
             }
         case SubtitleCellValue.email:
             let emailCount = contact.emails.count
